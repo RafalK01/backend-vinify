0a121c104a88d86e10e869bf21064b52e074b4af
Rafal Kolibaba <rafal.kolibaba@icloud.com>
2023-05-18 08:45:37 +0200

    fix support

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const wineRoutes = require('./routes/wine.routes')
 app.use('/api', wineRoutes)
 
 const supportRoutes = require('./routes/support.routes')
-app.use('/api', isAuthenticated, supportRoutes)
+app.use('/api', supportRoutes)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app)
